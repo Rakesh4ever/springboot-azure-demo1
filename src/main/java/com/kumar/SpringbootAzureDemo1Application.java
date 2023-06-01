7ff030a7a06b0dc986761d6fc57cdf272a4e710f
R Kumar <rkumar@Rs-MacBook-Air.local>
2023-06-01 06:57:32 +0530

    first commit

---
+++ src/main/java/com/kumar/SpringbootAzureDemo1Application.java
@@ -0,0 +1,13 @@
+package com.kumar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAzureDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAzureDemo1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/kumar/controller/AzureDemoController.java
@@ -0,0 +1,13 @@
+package com.kumar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AzureDemoController {
+
+	@GetMapping("/geMessage")
+	public String getConnectionWithAzure() {
+		return "Conneted to the Azure cloud : Deployed";
+	}
+}
