cac11962f2b8f9373e19be65ca4c8de0c9a7d405
R Kumar <rkumar@Rs-MacBook-Air.local>
2023-06-30 21:00:41 +0530

    Update in controller : test

--- src/main/java/com/kumar/controller/AzureDemoController.java
+++ src/main/java/com/kumar/controller/AzureDemoController.java
@@ -8,6 +8,6 @@ public class AzureDemoController {
 
 	@GetMapping("/geMessage")
 	public String getConnectionWithAzure() {
-		return "Conneted to the Azure cloud : Deployed successfully";
+		return "Conneted to the Azure cloud : Deployed successfully2";
 	}
 }
